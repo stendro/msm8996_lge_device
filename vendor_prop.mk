f5ab28fa35b106086a0654eba76404375dc39ba2
tangrobin <tangrobin@google.com>
2021-09-25 13:06:15 +0530

    msm8996-common: Use phase offsets as duration
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Adopt the new way of specifying
    phase offsets to SurfaceFlinger.
    
    Bug: 158464992
    Test: scrolling, app launching/closing, playing video
    Change-Id: Ia717f5c0075b6adaedaf20dd6be689737edc1415

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -118,7 +118,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.enable_vds=1 \
     sdm.debug.disable_rotator_split=1 \
     ro.surface_flinger.set_idle_timer_ms=9000 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    debug.sf.early.app.duration=16500000 \
+    debug.sf.early.sf.duration=21000000 \
+    debug.sf.earlyGl.app.duration=21000000 \
+    debug.sf.earlyGl.sf.duration=13500000 \
+    debug.sf.late.app.duration=20500000 \
+    debug.sf.late.sf.duration=10500000 \
+    debug.sf.use_phase_offsets_as_durations=1
 
 # HWUI
 PRODUCT_PROPERTY_OVERRIDES += \
