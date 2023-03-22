a5672a3bcf97b056bf8df821cc876fd0b8afa0b4
Nolen Johnson <johnsonnolen@gmail.com>
2023-03-22 22:06:21 +0100

    msm8996-common: Alphabetize board-flags and reorganize
    stendro: Adjust for my source. To be more in line with LOS.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,6 +23,11 @@ include $(COMMON_PATH)/lge/setup.txt
 # Inherit from common product
 -include device/$(PRODUCT_BRAND)/common/BoardConfigCommon.mk
 
+# Inherit from the proprietary version
+include vendor/$(PRODUCT_BRAND)/msm8996-common/BoardConfigVendor.mk
+include vendor/$(PRODUCT_BRAND)/$(LGE_DEVICE_FAMILY)-common/BoardConfigVendor.mk
+include vendor/$(PRODUCT_BRAND)/$(LGE_DEVICE)/BoardConfigVendor.mk
+
 ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
@@ -45,27 +50,6 @@ TARGET_BOARD_INFO_FILE := $(COMMON_PATH)/board-info.txt
 
 # OTA Assert
 TARGET_OTA_ASSERT_DEVICE := $(LGE_OTA),$(LGE_DEVICE)
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := MSM8996
-TARGET_NO_BOOTLOADER := true
-
-# HWUI
-HWUI_COMPILE_FOR_PERF := true
-
-# Kernel
-BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x237 lpm_levels.sleep_disabled=1
-BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.usbconfigfs=true
-BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=6a00000.dwc3
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
-BOARD_RAMDISK_USE_LZ4 := true
-TARGET_KERNEL_VERSION := 4.4
-TARGET_KERNEL_NEW_GCC_COMPILE := true
-TARGET_KERNEL_SOURCE := kernel/$(PRODUCT_BRAND)/msm8996
-TARGET_KERNEL_CONFIG := $(CUSTOM_VENDOR)os_$(LGE_DEVICE)_defconfig
 
 # Audio
 AUDIO_FEATURE_ENABLED_AAC_ADTS_OFFLOAD := true
@@ -106,15 +90,19 @@ BOARD_USES_ALSA_AUDIO := true
 USE_CUSTOM_AUDIO_POLICY := 1
 
 # Bluetooth/FM
-BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth/$(LGE_BCMCHIP)
+BOARD_CUSTOM_BT_CONFIG := $(COMMON_PATH)/bluetooth/libbt_vndcfg.txt
+BOARD_CUSTOM_FM_CONFIG := $(COMMON_PATH)/bluetooth/fm_vndcfg.txt
 BOARD_HAVE_BCM_FM := true
+BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_HAVE_LGE_BLUESLEEP := true
-BOARD_CUSTOM_FM_CONFIG := $(COMMON_PATH)/bluetooth/fm_vndcfg.txt
-BOARD_CUSTOM_BT_CONFIG := $(COMMON_PATH)/bluetooth/libbt_vndcfg.txt
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth/$(LGE_BCMCHIP)
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8996
+TARGET_NO_BOOTLOADER := true
 
 # Camera
 TARGET_LD_SHIM_LIBS := \
@@ -127,18 +115,18 @@ endif
 USE_CAMERA_STUB := true
 
 # Display
-TARGET_CONTINUOUS_SPLASH_ENABLED := true
-TARGET_USES_C2D_COMPOSITION := true
-TARGET_USES_ION := true
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-TARGET_USES_HWC2 := true
-TARGET_USES_GRALLOC1 := true
 ifeq ($(LGE_DEVICE_FAMILY),g6)
 TARGET_HAS_HDR_DISPLAY := true
 TARGET_HAS_WIDE_COLOR_DISPLAY := true
 endif
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_GRALLOC1 := true
+TARGET_USES_HWC2 := true
+TARGET_USES_ION := true
 
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
@@ -155,6 +143,23 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/lge/manifest-$(LGE_CODE_NAME).xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 TARGET_FS_CONFIG_GEN += $(COMMON_PATH)/config.fs
+
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x237 lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.usbconfigfs=true
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=6a00000.dwc3
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+BOARD_RAMDISK_USE_LZ4 := true
+TARGET_KERNEL_VERSION := 4.4
+TARGET_KERNEL_NEW_GCC_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/$(PRODUCT_BRAND)/msm8996
+TARGET_KERNEL_CONFIG := $(CUSTOM_VENDOR)os_$(LGE_DEVICE)_defconfig
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
@@ -186,7 +191,6 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_ROOT_EXTRA_SYMLINKS := /mnt/vendor/sns:/sns /mnt/vendor/mpt:/mpt
 
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
@@ -199,6 +203,13 @@ BOOTLOADER_MESSAGE_OFFSET := 128
 TARGET_RECOVERY_DEVICE_DIRS += $(COMMON_PATH)
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm8996
+
+# Root symlinks
+BOARD_ROOT_EXTRA_SYMLINKS := /mnt/vendor/sns:/sns /mnt/vendor/mpt:/mpt
+
 # SELinux policies
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
@@ -206,22 +217,13 @@ BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/lge/sepolicy/$(LGE_CODE_NAME)
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Wi-Fi
-WPA_SUPPLICANT_VERSION      := VER_0_8_X
-BOARD_WLAN_DEVICE           := bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER        := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
-
-# Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
-TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm8996
-
-# Inherit from the proprietary version
-include vendor/$(PRODUCT_BRAND)/msm8996-common/BoardConfigVendor.mk
-include vendor/$(PRODUCT_BRAND)/$(LGE_DEVICE_FAMILY)-common/BoardConfigVendor.mk
-include vendor/$(PRODUCT_BRAND)/$(LGE_DEVICE)/BoardConfigVendor.mk
+WPA_SUPPLICANT_VERSION := VER_0_8_X
